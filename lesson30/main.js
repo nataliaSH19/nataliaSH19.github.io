d06541428da7b5e7b760a9df1e8c438e9881692d
Natali <nataliashelenko2@gmail.com>
2020-05-06 18:28:01 +0300

    edit lesson30(task2-3)

--- lesson30/main.js
+++ lesson30/main.js
@@ -25,3 +25,46 @@ function keyHandler(e) {
     someText.style.display = "block";
   }
 }
+//task2
+document.addEventListener("DOMContentLoaded", () => {
+  const getSort = ({ target }) => {
+    const order = (target.dataset.order = -(target.dataset.order || -1));
+    const index = [...target.parentNode.cells].indexOf(target);
+    const collator = new Intl.Collator(["en", "ru"], { numeric: true });
+    const comparator = (index, order) => (a, b) =>
+      order *
+      collator.compare(
+        a.children[index].innerHTML,
+        b.children[index].innerHTML
+      );
+
+    for (const tBody of target.closest("table").tBodies)
+      tBody.append(...[...tBody.rows].sort(comparator(index, order)));
+
+    for (const cell of target.parentNode.cells)
+      cell.classList.toggle("sorted", cell === target);
+  };
+
+  document
+    .querySelectorAll(".table_sort thead")
+    .forEach((tableTH) =>
+      tableTH.addEventListener("click", () => getSort(event))
+    );
+});
+//task3
+const resizeTable = document.querySelector(".res_table");
+const resizeBtn = document.querySelector(".res_btn");
+function resize(event) {
+  resizeTable.style.width =
+    event.clientX - resizeTable.getBoundingClientRect().left + "px";
+  resizeTable.style.height =
+    event.clientY - resizeTable.getBoundingClientRect().top + "px";
+}
+function stopResize() {
+  document.removeEventListener("mousemove", resize);
+}
+
+resizeBtn.addEventListener("mousedown", () => {
+  document.addEventListener("mousemove", resize);
+});
+document.addEventListener("mouseup", stopResize);
